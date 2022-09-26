61adceaabb7e67811b84ab394a3d30ee66d2524c
90Niko <110725784+90Niko@users.noreply.github.com>
2022-09-26 12:46:21 +0200

    g
    g

---
+++ Basic Syntax-Exercise/Back in 30 Minutes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Back__in__30__Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine()) +30;
+
+            if (minutes > 59 )
+            {
+                minutes -= 60;
+                hours = hours + 1;
+                
+            }
+            if (hours > 23)
+            {
+                hours = 0;
+            }
+
+            Console.WriteLine($"{hours}:{minutes:D2} ");
+           
+            
+            
+
+        }
+    }
+}
